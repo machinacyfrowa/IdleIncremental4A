6d77b2c0ba02d7077c22c6e8886d66dbe82c6cc8
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2026-05-13 10:24:52 +0200

    growing "plant"

---
+++ Assets/CropController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CropController : MonoBehaviour
+{
+    GameObject model;
+    float growth; //wyroœniêcie od 0 do 1
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //przypisz pierwszy obiekt potomny jako model roœliny
+        model = transform.GetChild(0).gameObject;
+        growth = 0.01f; //bardzo ma³a ale nie zerowa
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(growth < 1f)
+        {
+            //dodaj wzost - 0.1 na sekundê
+            growth += Time.deltaTime * 0.1f; //roœnie o 0.1 na sekundê
+            //growth /2 bo tymczasowy model jest 2 razy wiêkszy ni¿ roœlina
+            model.transform.localScale = new Vector3(growth/2, growth/2, growth/2);
+        }
+        
+    }
+}
